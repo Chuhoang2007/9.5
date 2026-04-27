c52134d648a407f55a5ab644cdb132cb9ae6865a
Chuhoang2007 <25023476@vnu.edu.vn>
2026-04-27 18:11:08 +0700

    Commit lan 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static int tinhTong(int a, int b) {
+        logger.info("Đang tính tổng của {} và {}", a, b);
+        return a + b;
+    }
+    public static int tinhHieu(int a, int b) {
+        logger.info("Đang tính hiệu của {} và {}", a, b);
+        return a - b;
+    }
+    public static void main(String[] args) {
+        System.out.println("Tổng là: " + tinhTong(5, 5));
+    }
+}
